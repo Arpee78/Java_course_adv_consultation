a19ad1e76c92a45f099b8faa89e49783b4a2adce
Arpee78 <arpee78@gmail.com>
2023-02-16 07:55:21 +0100

    w01d3 improved

--- src/main/java/w01d2/Algorithms.java
+++ src/main/java/w01d2/Algorithms.java
@@ -1,30 +1,32 @@
 package w01d2;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Algorithms {
 
     //Készíts egy osztályt Algorithms néven, benne egy metódussal, ami egész számok listáját várja.
     // Tudjuk, hogy a listában minden szám egyszer szerepel, kivéve egyet.
     // A feladat az, hogy add vissza azt a számot amelyik többször szerepel!
-    public int findDuplicatedNumberS1(List<Integer> numbers){
-        Collections.sort(numbers);
-        for (int i = 0; i < numbers.size()-1; i++) {
-            if (numbers.get(i)==numbers.get(i+1)){
-                return numbers.get(i);
+
+    public int findDuplicatedNumber_sort(int[] numbers){
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length-1; i++) {
+            if (numbers[i]==numbers[i+1]){
+                return numbers[i];
             }
         }
-        return 0;
+        return numbers.length;
     }
 
-    public int findDuplicatedNumber(List<Integer> numbers){
-        Collections.sort(numbers);
-        for (int i = 0; i < numbers.size()-1; i++) {
-            if (numbers.get(i)==numbers.get(i+1)){
-                return numbers.get(i);
+    public int findDuplicate_set(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (!set.add(numbers[i])) {
+                return numbers[i];
             }
         }
-        return 0;
-
+        return numbers.length;
     }
+
 }
--- src/test/java/w01d2/AlgorithmsTest.java
+++ src/test/java/w01d2/AlgorithmsTest.java
@@ -1,5 +1,7 @@
 package w01d2;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +9,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AlgorithmsTest {
 
-    @org.junit.jupiter.api.Test
-    void findDuplicatedNumber() {
+    @Test
+    void findDuplicatedNumber_sort() {
         Algorithms algorithms = new Algorithms();
-        List<Integer> listOfNumbers = new ArrayList<>(List.of(6,2,5,4,3,3,1));
-        int result = algorithms.findDuplicatedNumber(listOfNumbers);
-        assertEquals(2, result);
+        int[] numbers = {10, 2, 5, 4, 3, 1, 1};
+        int result = algorithms.findDuplicatedNumber_sort(numbers);
+        assertEquals(1, result);
+    }
+
+    @Test
+    void findDuplicatedNumber_set() {
+        Algorithms algorithms = new Algorithms();
+        int[] numbers = {10, 2, 5, 4, 3, 1, 1};
+        int result = algorithms.findDuplicate_set(numbers);
+        assertEquals(1, result);
     }
 
 }
